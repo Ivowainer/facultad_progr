f8f2c2c268df4b2fd536e1e84e5ebf1ce0079434
Ivowainer <ivancamposwainer@gmail.com>
2025-04-01 18:33:26 -0300

    EJS

--- so1/p1/ej2/f.c
+++ so1/p1/ej2/f.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <wait.h>
+#include <sys/wait.h>
 
-int main(){
-    int *a = malloc(1*1000*1000*1000);
-    for(int i = 0; i<1*1000*1000*100;i++){
+int main()
+{
+    int *a = malloc(1 * 1000 * 1000 * 1000);
+    for (int i = 0; i < 1 * 1000 * 1000 * 100; i++)
+    {
         a[i] = 320;
-        //sleep(1);
+        // sleep(1);
     }
-    while(1);
+    while (1)
+        ;
     return 0;
 }
---
+++ so1/p1/ej7/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+int main()
+{
+    printf("hola\n");
+    return 0;
+}
