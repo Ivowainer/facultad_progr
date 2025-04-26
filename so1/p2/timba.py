e31115dae0472bc48c2e14f12df62cf29dc95f81
Ivowainer <ivancamposwainer@gmail.com>
2025-04-26 16:15:50 -0300

    Timba SO1 p1

---
+++ so1/p2/timba.py
@@ -0,0 +1,43 @@
+import random
+
+preguntas = [
+    "¿Cuál es la vida de un proceso?",
+    "¿Qué es un cambio de contexto?",
+    "¿Qué es un Sistema Operativo?",
+    "¿Qué hace la llamada fork()?",
+    "¿Qué es un pipe?",
+    "¿Qué es un socket?",
+    "¿Qué es la exclusión mutua?",
+    "Explicá race condition, operación atómica y sección crítica.",
+    "¿Qué es la propiedad de safety/liveness?",
+    "¿Qué es un deadlock?",
+    "¿Qué es un thread?",
+    "¿Qué diferencias hay entre un proceso y un hilo?",
+    "¿Qué es la memoria compartida?",
+    "¿Qué mecanismos de sincronización conoces?",
+    "¿Qué es busy waiting?",
+    "¿Cómo funciona el algoritmo de Peterson?",
+    "¿Cómo funciona el algoritmo de la Panadería de Lamport?",
+    "¿Qué pasa si dos procesos escriben en el mismo archivo sin sincronización?",
+    "¿Qué tipos de estados puede tener un proceso?",
+    "¿Qué ventajas tiene usar threads sobre procesos?",
+    "¿Que es la concurrencia y que diferencia tiene con el paralelismo?",
+    "¿Que es Store Buffering?",
+    "¿Que es Fence (Barrera de memoria)?",
+    "¿Para que sirve volatile?",
+    "Nombrar instrucciones de pthread mutex"
+]
+
+def timba_preguntas():
+    random.shuffle(preguntas)
+
+    while preguntas:
+        input("Apreta enter")
+        pregunta = preguntas.pop()
+        print(pregunta)
+
+    print("god")
+
+if __name__ == "__main__":
+    timba_preguntas()
+
